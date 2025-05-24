9be576c5309fab631e3f82d80b98aae8ca57caba
Ivan Martinez <ivanmartinezm09@gmail.com>
2025-05-24 10:14:43 +0200

    fix:lint errors

--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,9 +1,8 @@
-import { expect, afterEach, vi } from 'vitest';
-import { cleanup } from '@testing-library/react';
-import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect, vi } from "vitest";
+import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
-vi.mock('global', () => ({
-  fetch: vi.fn()
+vi.mock("global", () => ({
+  fetch: vi.fn(),
 }));
