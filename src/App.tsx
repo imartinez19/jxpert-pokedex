3c3b97bf5beb96845101b9e504e5de794cdbeea4
Daniel Garcia <danigarciaberzal@gmail.com>
2025-05-31 12:04:29 +0200

    refactor: magic variables and typing

--- src/App.tsx
+++ src/App.tsx
@@ -82,7 +82,11 @@ const STATS: Stats = {
   SPECIAL_DEFENSE: "special-defense",
   SPEED: "speed",
 };
-const DEFAULT_SORT = "default";
+const DEFAULT_SORT: string = "default";
+
+const LOADING_PLACEHOLDER_LENGTH: number = 6;
+const MAX_STAT_VALUE: string = "255";
+const NUMBER_LENGTH: number = 3;
 
 export const App = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -585,19 +589,22 @@ export const App = () => {
         <section>
           {(loading || filtering) && !showListError && (
             <div className="grid" aria-hidden="true">
-              {Array.from({ length: 6 }, (_, index) => {
-                return (
-                  <article
-                    key={`placeholder-card-${index}`}
-                    className="card card-placeholder"
-                    data-testid="card-placeholder"
-                  >
-                    <svg viewBox="0 0 24 24">
-                      <path d="M12,2C17.52,2 22,6.48 22,12C22,17.52 17.52,22 12,22C6.48,22 2,17.52 2,12C2,6.48 6.48,2 12,2M12,4C7.92,4 4.55,7.05 4.06,11H8.13C8.57,9.27 10.14,8 12,8C13.86,8 15.43,9.27 15.87,11H19.94C19.45,7.05 16.08,4 12,4M12,20C16.08,20 19.45,16.95 19.94,13H15.87C15.43,14.73 13.86,16 12,16C10.14,16 8.57,14.73 8.13,13H4.06C4.55,16.95 7.92,20 12,20M12,10C10.9,10 10,10.9 10,12C10,13.1 10.9,14 12,14C13.1,14 14,13.1 14,12C14,10.9 13.1,10 12,10Z" />
-                    </svg>
-                  </article>
-                );
-              })}
+              {Array.from(
+                { length: LOADING_PLACEHOLDER_LENGTH },
+                (_, index) => {
+                  return (
+                    <article
+                      key={`placeholder-card-${index}`}
+                      className="card card-placeholder"
+                      data-testid="card-placeholder"
+                    >
+                      <svg viewBox="0 0 24 24">
+                        <path d="M12,2C17.52,2 22,6.48 22,12C22,17.52 17.52,22 12,22C6.48,22 2,17.52 2,12C2,6.48 6.48,2 12,2M12,4C7.92,4 4.55,7.05 4.06,11H8.13C8.57,9.27 10.14,8 12,8C13.86,8 15.43,9.27 15.87,11H19.94C19.45,7.05 16.08,4 12,4M12,20C16.08,20 19.45,16.95 19.94,13H15.87C15.43,14.73 13.86,16 12,16C10.14,16 8.57,14.73 8.13,13H4.06C4.55,16.95 7.92,20 12,20M12,10C10.9,10 10,10.9 10,12C10,13.1 10.9,14 12,14C13.1,14 14,13.1 14,12C14,10.9 13.1,10 12,10Z" />
+                      </svg>
+                    </article>
+                  );
+                },
+              )}
             </div>
           )}
           {/* Prints cards */}
@@ -613,7 +620,9 @@ export const App = () => {
                     <article className="card" style={customStyles}>
                       <header className="card__head">
                         <div className="card__tag">
-                          <p>#{res.id.toString().padStart(3, "0")}</p>
+                          <p>
+                            #{res.id.toString().padStart(NUMBER_LENGTH, "0")}
+                          </p>
                         </div>
                         <div className="card__tag">
                           <img
@@ -650,7 +659,7 @@ export const App = () => {
                             </div>
                             <progress
                               value={res.stats[0].base_stat}
-                              max="255"
+                              max={MAX_STAT_VALUE}
                             ></progress>
                           </li>
                           <li className="card__stat" aria-label="Attack">
@@ -662,7 +671,7 @@ export const App = () => {
                             </div>
                             <progress
                               value={res.stats[1].base_stat}
-                              max="255"
+                              max={MAX_STAT_VALUE}
                             ></progress>
                           </li>
                           <li className="card__stat" aria-label="Defense">
@@ -674,7 +683,7 @@ export const App = () => {
                             </div>
                             <progress
                               value={res.stats[2].base_stat}
-                              max="255"
+                              max={MAX_STAT_VALUE}
                             ></progress>
                           </li>
                           <li
@@ -689,7 +698,7 @@ export const App = () => {
                             </div>
                             <progress
                               value={res.stats[3].base_stat}
-                              max="255"
+                              max={MAX_STAT_VALUE}
                             ></progress>
                           </li>
                           <li
@@ -704,7 +713,7 @@ export const App = () => {
                             </div>
                             <progress
                               value={res.stats[4].base_stat}
-                              max="255"
+                              max={MAX_STAT_VALUE}
                             ></progress>
                           </li>
                           <li className="card__stat" aria-label="Speed">
@@ -716,7 +725,7 @@ export const App = () => {
                             </div>
                             <progress
                               value={res.stats[5].base_stat}
-                              max="255"
+                              max={MAX_STAT_VALUE}
                             ></progress>
                           </li>
                         </ul>
